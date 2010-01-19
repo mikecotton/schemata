d81e12e5d2c0a20bbedc6491d15382ea68ac4966
Michael Cotton <mike@Michael-Cottons-MacBook-Pro-15.local>
2010-01-18 17:53:01 -0800

    prod-worthy start and stop scripts

---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+paster serve --daemon --log-file=paster.log --pid-file=paster.pid --reload development.ini start
---
+++ stop.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+paster serve --daemon --log-file=paster.log --pid-file=paster.pid --reload development.ini stop
+
